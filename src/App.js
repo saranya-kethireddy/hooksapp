2c3a6a61ed0d74796d3b3b37a95f90b5292b1e4c
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-18 12:54:12 +0530

    context for passing data between different components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import DataFetching from './components/hooks/DataFetching';
+import DataFetching from './components/useeffecthooks/DataFetching';
+import ComponentA from './components/contexthooks/ComponentA';
+import React from 'react';
+
+export const UserContext = React.createContext()
+
 
 function App() {
   return (
     <div className="App">
 
-      
-      <DataFetching/>
+
+      <UserContext.Provider value={'saranya'} >
+        <ComponentA />
+      </UserContext.Provider>
     </div>
   );
 }
