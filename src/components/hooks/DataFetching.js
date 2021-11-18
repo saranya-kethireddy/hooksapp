94d68ee999813c1a0bac4d4a61c245a4751c880c
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-18 12:32:23 +0530

    getting only one post

--- src/components/hooks/DataFetching.js
+++ src/components/hooks/DataFetching.js
@@ -2,31 +2,36 @@ import React, {useState,useEffect} from 'react'
 import axios from 'axios'
 
 function DataFetching() {
-    const [posts,setPosts] = useState([])
+    const [post,setPost] = useState({})
+        const [id,setId] = useState(1)
 
     useEffect(
         () => {
-            axios.get('https://jsonplaceholder.typicode.com/posts')
+            axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then( res => {
                 console.log(res)
-                setPosts(res.data)
+                setPost(res.data)
             })
             .catch(err => {
                 console.log(err)
             })
         },
-        []
+        [id]
     )
 
 
 
     return (
         <div>
-            <ul>{
-                posts.map(
-                    post => <li key = {post.id}>{post.title}</li>
-                )
-            }</ul>
+        <input 
+            type = "text"
+            value = {id}
+            onChange = { e => setId(e.target.value)}
+            />
+            <div>{post.title}</div>
+        
+        
+       
         </div>
     )
 }
